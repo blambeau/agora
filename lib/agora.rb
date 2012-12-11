e81866fe221155f8decfd8b1d0fe6f5fc3956c95
Bernard Lambeau <blambeau@gmail.com>
2012-12-11 11:47:16 +0100

    Remove Config object.

--- lib/agora.rb
+++ lib/agora.rb
@@ -1,17 +1,10 @@
 require 'agora/version'
 require 'agora/loader'
-require 'pathname'
 #
 # Agile Goal-Oriented Requirement Acquisition
 #
 module Agora
 
-  # Loads Agora in a given location
-  def self.load(root = ".")
-    config = Config.find(root)
-  end
-
 end # module Agora
 require "agora/goal_model"
-require "agora/config"
 require "agora/utils"
